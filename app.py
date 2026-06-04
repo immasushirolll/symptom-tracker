03d3434ef77f61032b3655414fb36b415c0235b6
immasushiroll <jane8911669@gmail.com>
2026-06-03 21:49:58 -0400

    added processing and whisper pipeline

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import json
 from datetime import datetime
+import subprocess
 
 app = Flask(__name__)
 
 AUDIO_DIR = os.path.join(os.path.dirname(__file__), "static", "audio")
 METADATA_FILE = os.path.join(os.path.dirname(__file__), "static", "audio", "metadata.json")
+AUDIO_DATA_FILE = os.path.join(os.path.dirname(__file__), "static", "audio", "input.wav.txt")
 
 os.makedirs(AUDIO_DIR, exist_ok=True)
 
@@ -17,11 +19,21 @@ def load_metadata():
             return json.load(f)
     return []
 
-
 def save_metadata(data):
     with open(METADATA_FILE, "w") as f:
         json.dump(data, f, indent=2)
 
+def load_audio_data():
+    """Load STT extraction file."""
+    if os.path.exists(AUDIO_DATA_FILE):
+        with open(AUDIO_DATA_FILE, "r") as f:
+            return json.load(f)
+    return []
+
+def save_audio_data(data):
+    """Save STT extraction file."""
+    with open(AUDIO_DATA_FILE, "a") as f:
+        f.write(data)
 
 # ── Pages ──────────────────────────────────────────────────────────────────────
 
@@ -54,7 +66,7 @@ def admin():
 
 @app.route("/api/save-recording", methods=["POST"])
 def save_recording():
-    """Save an audio recording sent from the browser."""
+    """Save an audio recording with the STT transcript sent from the browser."""
     if "audio" not in request.files:
         return jsonify({"error": "No audio file provided"}), 400
 
@@ -64,7 +76,8 @@ def save_recording():
     name = request.form.get("name", "Friend").strip()
 
     timestamp = datetime.now()
-    filename = f"checkin_{timestamp.strftime('%Y%m%d_%H%M%S')}.webm"
+    filename = f"input_{timestamp.strftime('%Y%m%d_%H%M%S')}.webm"
+    wav_filename = filename.replace(".webm", ".wav")
     filepath = os.path.join(AUDIO_DIR, filename)
     audio_file.save(filepath)
 
@@ -82,6 +95,21 @@ def save_recording():
     }
     metadata.append(entry)
     save_metadata(metadata)
+
+    conversion_output = subprocess.call([
+        "ffmpeg", "-i", f"/home/immasushiroll/Windows/Users/jane8/repos/symptom-tracker/static/audio/{filename}", "-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le", f"/home/immasushiroll/Windows/Users/jane8/repos/symptom-tracker/static/audio/{wav_filename}"
+    ])
+
+    text_output = subprocess.call([
+        "/home/immasushiroll/Windows/Users/jane8/repos/whisper.cpp/build/bin/whisper-cli",
+        "-m", "/home/immasushiroll/Windows/Users/jane8/repos/whisper.cpp/models/ggml-base.en.bin",
+        "-f", f"/home/immasushiroll/Windows/Users/jane8/repos/symptom-tracker/static/audio/{wav_filename}",
+        "-otxt"
+    ])
+
+    print(f"\Success=0, Fail=1 for {filename}:{text_output}\n")
+
+    # save_audio_data(text_output)
 
     return jsonify({"success": True, "entry": entry})
 
